40d7ea742b067502add3bee726fd2eea8e795f22
Michael Miklis <info@michaelmiklis.de>
2018-04-23 16:02:54 +0200

    Initial Check-in
    Created script and upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Start-AADAttributeWriteback.ps1
+
+This PowerShell script can be used for attribute writeback from Azure Active Directory (AAD) to the on-premises Active Directory (AD).
+Only accounts which are synchronized by Azure AD Connect (ImmutabledID attribute present) are being processed. The following AAD attributes
+will be written to the corosponding on-premises AD account:
+
+- targetaddress
+- msExchHomeServerName
+- legacyExchangeDN 
+- mailNickName
+
+Once these attributes are present in the on-premises AD user accounts, GALSync can be used to synchronize these users to another
+Active Directory as contact objects.
+
+This will be very usefull if you have multiple Active Directory / Exchange Environments and some ADs are using Exchange online only.
+
+
+Usage
+========
+
+	Start-AADAttributeWriteback.ps1 -Credential (Get-Credential)
---
+++ Start-AADAttributeWriteback.ps1
@@ -0,0 +1,122 @@
+﻿######################################################################
+## (C) 2018 Michael Miklis (michaelmiklis.de)
+##
+##
+## Filename:      Start-AADAttributeWriteback.ps1
+##
+## Version:       1.0
+##
+## Release:       Final
+##
+## Requirements:  -none-
+##
+## Description:   Synchronizes Exchange attributes from Exchange Online
+##                user account to the on-premises AD account.
+##                
+##                That will enable the use of Microsoft Identity Manager
+##                GALSync module.
+##
+## This script is provided 'AS-IS'.  The author does not provide
+## any guarantee or warranty, stated or implied.  Use at your own
+## risk. You are free to reproduce, copy & modify the code, but
+## please give the author credit.
+##
+####################################################################
+
+param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()][object]$Credential
+)
+
+
+Set-PSDebug -Strict
+Set-StrictMode -Version latest
+  
+ 
+function Start-AADAttributeWriteback
+{
+    <#
+        .SYNOPSIS
+        Synchronize targetaddress, msExchHomeServerName, legacyExchangeDN 
+        and mailNickName from AAD to AD
+  
+        .DESCRIPTION
+        The Start-AADAttributeWriteback CMDlet gets the attributes targetaddress, 
+        msExchHomeServerName, legacyExchangeDN and mailNickName and writes the
+        attribute's values to the on-premises Active Directory account
+  
+        .PARAMETER Credential
+        Credential to log on to Office 365 / Exchange online
+  
+        .EXAMPLE
+        Start-AADAttributeWriteback -Credential (Get-Credential)
+ 
+    #>
+
+    param (
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()][object]$Credential
+    )
+
+    # Connect to Office 365 
+    Connect-MsolService -Credential $Credential
+
+    # Connect to Exchange online PowerShell interface
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication  Basic -AllowRedirection
+
+    # Import Exchange online PSSession
+    Import-PSSession $Session
+
+    # Loop for each Exchange online Mailbox / User
+    foreach ($TenantUser in $(Get-Mailbox | Select LegacyExchangeDN,ServerLegacyDN,PrimarySMTPAddress, UserPrincipalName, Alias))
+    {
+
+        # Get AzureAD Account for ImmutableID
+        try
+        {
+            $AzureADUser = Get-MSOLUser -UserPrincipalName $TenantUser.UserPrincipalName
+        }
+        catch
+        {
+            $AzureADUser = $null
+            Continue
+        }
+
+        # Only process synchronized accounts
+        if ($AzureADUser.ImmutableID -ne $null)
+        {
+            # Convert ImmutableID into GUID format
+            [GUID]$decodedGUID = [system.convert]::frombase64string($AzureADUser.ImmutableID)
+        
+            # Search Active Directory user based on GUID
+            try
+            {
+                $ADUser = Get-AdObject -Identity $decodedGUID
+            }
+            catch
+            {
+                write-warning ("No on-premise account found for Azure-AD user {0}" -f $TenantUser.UserPrincipalName)
+                $ADUser = $null
+            }
+
+            # if ADUser was found continue
+            if ($ADUser -ne $null)
+            {
+               Set-ADObject -Identity $decodedGUID -Replace @{targetaddress=("SMTP:"+$TenantUser.PrimarySmtpAddress)}
+           
+               Set-ADObject -Identity $decodedGUID -Replace @{msExchHomeServerName =$TenantUser.ServerLegacyDN}
+
+               Set-ADObject -Identity $decodedGUID -Replace @{legacyExchangeDN =$TenantUser.LegacyExchangeDN}
+
+               Set-ADObject -Identity $decodedGUID -Replace @{mailNickName =$TenantUser.Alias}
+            }
+
+        }
+    }
+
+    # Disconnect Exchange online PSSession
+    Remove-PSSession $Session
+}
+
+
+Start-AADAttributeWriteback -Credential $Credential
